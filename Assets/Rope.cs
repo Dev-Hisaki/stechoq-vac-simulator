ee50e47927c2008104c11b877dacf22132fedfd1
Muhamad Haidar Al-Farruq <muharuichijou@gmail.com>
2024-10-22 15:41:02 +0700

    Fix: ropes (not fixed yet)

--- Assets/Rope.cs
+++ Assets/Rope.cs
@@ -1,26 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rope : MonoBehaviour
 {
-    public Transform startObject;  // Objek awal
-    public Transform endObject;    // Objek akhir
-    public Transform rope;         // Cylinder untuk tali
-
-    void Update()
+    public GameObject startPoint, endPoint;
+    // public GameObject[] segmentPoints;
+    private LineRenderer line;
+    private void Start()
     {
-        // Hitung posisi tengah di antara dua objek
-        Vector3 middlePosition = (startObject.position + endObject.position) / 2;
-        rope.position = middlePosition;
-
-        // Hitung jarak antara dua objek
-        float distance = Vector3.Distance(startObject.position, endObject.position);
-
-        // Setel skala cylinder agar sesuai panjangnya
-        rope.localScale = new Vector3(rope.localScale.x, distance / 2, rope.localScale.z);
-
-        // Rotasi tali agar menghadap dari satu objek ke objek lainnya
-        rope.LookAt(endObject.position);
+        line = GetComponent<LineRenderer>();
+        line.SetPosition(0, startPoint.transform.position);
+        line.SetPosition(1, endPoint.transform.position);
+        // int segmentSize = segmentPoints.Length;
+        // for (int i = 0; i < segmentPoints.Length; i++)
+        // {
+        //     Debug.Log($"Line {i} set");
+        //     line.SetPosition(i, segmentPoints[i].transform.position);
+        // }
     }
 }
