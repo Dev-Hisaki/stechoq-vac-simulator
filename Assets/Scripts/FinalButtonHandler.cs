08901b07cb13eea09c2e21fc931e6ec5420e4883
Muhamad Haidar Al-Farruq <muharuichijou@gmail.com>
2024-12-06 20:51:33 +0700

    Add: idk

--- Assets/Scripts/FinalButtonHandler.cs
+++ Assets/Scripts/FinalButtonHandler.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class FinalButtonHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject finishPanel;
+    private void Start()
+    {
+        if (finishPanel.activeSelf) finishPanel.SetActive(false);
+    }
+    public void OpenFinishPanel()
+    {
+        finishPanel.SetActive(true);
+    }
     public void BackToMenu()
     {
         SceneManager.LoadScene(0);
